a05c7d0e9f74a3c3d719fbd4747a1c35d95d1373
manojkum157 <nutanapati.manoj@gmail.com>
2022-03-30 17:34:45 +0530

    manoj commits

--- angularapp/src/app/Admin/admincourse/editcourse/editcourse.component.ts
+++ angularapp/src/app/Admin/admincourse/editcourse/editcourse.component.ts
@@ -31,7 +31,7 @@ export class EditcourseComponent implements OnInit {
     )
   }
 
-  logout() {
+  LogoutSession() {
     sessionStorage.removeItem('value');
     this.route.navigateByUrl('/login')
   }
